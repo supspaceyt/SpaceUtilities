bd1fae3579356b818263ba73076dda1a514e5240
SupSpace <108158222+supspaceyt@users.noreply.github.com>
2022-06-24 18:32:22 -0500

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const { keep_alive } = require("./keep_alive");
-const Aoijs = require("aoi.js");
-const { Client, Collection, MessageEmbed } = require("discord.js");
-const fs = require("fs");
-const path = require("path");
-const config = require('./config.js');
-const bot = new Aoijs.Bot(config.Bot);
