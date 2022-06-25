f6b53949b61db610f9822048edc7005f0c49d4cd
SupSpace <108158222+supspaceyt@users.noreply.github.com>
2022-06-24 21:54:35 -0500

    Create welcome2.js

---
+++ src/commands/welcome/welcome2.js
@@ -0,0 +1,10 @@
+module.exports = ({
+name: "join2",
+$if : "v4",
+channel : "921481919974227988",
+code: `$color[1;#0422f7]
+$title[1;**Welcome to $serverName[$guildID]**]
+$image[1;https://media.discordapp.net/attachments/966494470646415400/989984639861026866/Add_a_heading_4.png]
+$description[1;<@$authorID> Welcome to $serverName[$guildID] Don't forget to check out <#886819741371609138> <#933103063294681138>]`,
+type: 'join'
+})
