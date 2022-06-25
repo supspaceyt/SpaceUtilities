2be4a128adad84a6829973b119b4dbd9d6c4989f
SupSpace <108158222+supspaceyt@users.noreply.github.com>
2022-06-24 21:08:47 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,3 +5,6 @@ const fs = require("fs");
 const path = require("path");
 const config = require('./config.js');
 const bot = new Aoijs.Bot(config.Bot);
+const loader = new Aoijs.LoadCommands(bot);
+loader.load(bot.cmd,'./src/commands/')
+require('./colors.js')(loader)
