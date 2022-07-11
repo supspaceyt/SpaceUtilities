c94c14351f288828de19b53de8a6f3ccacc07f60
SupSpace <108158222+supspaceyt@users.noreply.github.com>
2022-07-10 22:08:16 -0500

    Update welcome.js

--- src/commands/welcome/welcome.js
+++ src/commands/welcome/welcome.js
@@ -1,10 +1,11 @@
 module.exports = ({
-name: "join",
-$if : "v4",
-channel : "922342825662758962",
-code: `$color[1;#0422f7]
-$title[1;**Welcome to $serverName[$guildID]**]
-$image[1;https://media.discordapp.net/attachments/966494470646415400/989984639861026866/Add_a_heading_4.png]
-$description[1;<@$authorID> Welcome to $serverName[$guildID] Don't forget to check out <#886819741371609138> <#933103063294681138>]`,
-type: 'join'
-})
+    name: "join",
+    $if : "v4",
+    channel : "922342825662758962",
+    code: `$color[1;#0422f7]
+    $title[1;**Welcome to $serverName[$guildID]**]
+    $image[1;https://media.discordapp.net/attachments/966494470646415400/989984639861026866/Add_a_heading_4.png]
+    $description[1;Hello<@$authorID>,
+    Welcome to $serverName[$guildID] Don't forget to check out <#886819741371609138> <#933103063294681138>]`,
+    type: 'join'
+    })
