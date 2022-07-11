8ce8f6ee60edd2adbc345abb42580d9938378f17
SupSpace <108158222+supspaceyt@users.noreply.github.com>
2022-07-10 22:03:42 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
-const { keep_alive } = require("./keep_alive");
 const Aoijs = require("aoi.js");
+
+const os = require("os");
+
 const { Client, Collection, MessageEmbed } = require("discord.js");
+
 const fs = require("fs");
+
 const path = require("path");
-const config = require('./config.js');
+
+require("dotenv").config({
+	path: path.resolve(__dirname, '../.env'),
+});
+
+const config = require('./config');
+
 const bot = new Aoijs.Bot(config.Bot);
+
+bot.status(...require('./handler/status'));
+
+require('./events/callbacks')(bot);
+
+
+require('./events/ready')(bot);
+
 const loader = new Aoijs.LoadCommands(bot);
 loader.load(bot.cmd,'./src/commands/')
-require('./colors.js')(loader)
+
+require('./colors')(loader)
+
 const files = fs.readdirSync('./src/prev').filter(file => file.endsWith('.js'))
 files.forEach(x => {
-  require(`./prev/${x}`)(bot)
+require(`./prev/${x}`)(bot)
 });
