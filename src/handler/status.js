6afe3595e35a576ddc32923b577c463eb1eeea3c
SupSpace <108158222+supspaceyt@users.noreply.github.com>
2022-07-10 22:06:26 -0500

    Update status.js

--- src/handler/status.js
+++ src/handler/status.js
@@ -1,61 +1,61 @@
 module.exports = [{
-text: "For staff apps",
-type: "WATCHING",
-time: 12
-},{
-text: "Sup\'s videos",
-type: "WATCHING",
-time: 12
-},{
-text: "The mods",
-type:"WATCHING",
-time: 12
-},{
-text: "Hello",
-type: "WATCHING",
-time: 12
-},{
-text: "Yo",
-type: "PLAYING",
-time: 12
-},{
-text: "Call of duty",
-type: "PLAYING",
-time: 12
-},{
-text: "Rulebreakers",
-type: "WATCHING",
-time: 12
-},{
-text: "With my dog",
-type: "PLAYING",
-time: 12
-},{
-text: "Joe",
-type: "WATCHING",
-time: 12
-},{
-text: "Spotify",
-type: "LISTENING",
-time: 12
-},{
-text: "Smooth jazz",
-type: "LISTENING",
-time: 12
-},{
-text: "The latest covid-19 info",
-type: "WATCHING",
-time: 12
-},{
-text: "Join the discord! discord.gg/vJzqtew6nq",
-type: "PLAYING",
-time: 12
-},{
-text: "The world burn and come back",
-type: "WATCHING",
-time: 12
-},{
-text: "News about covid-19",
-type: "LISTENING",
-time: 12
-}]
+    text: "For staff apps",
+    type: "WATCHING",
+    time: 12
+    },{
+    text: "Sup\'s videos",
+    type: "WATCHING",
+    time: 12
+    },{
+    text: "The mods",
+    type:"WATCHING",
+    time: 12
+    },{
+    text: "Hello",
+    type: "WATCHING",
+    time: 12
+    },{
+    text: "Yo",
+    type: "PLAYING",
+    time: 12
+    },{
+    text: "Call of duty",
+    type: "PLAYING",
+    time: 12
+    },{
+    text: "Rule Breakers",
+    type: "WATCHING",
+    time: 12
+    },{
+    text: "With my dog",
+    type: "PLAYING",
+    time: 12
+    },{
+    text: "Joe",
+    type: "WATCHING",
+    time: 12
+    },{
+    text: "Spotify",
+    type: "LISTENING",
+    time: 12
+    },{
+    text: "Smooth jazz",
+    type: "LISTENING",
+    time: 12
+    },{
+    text: "The latest covid-19 info",
+    type: "WATCHING",
+    time: 12
+    },{
+    text: "Join the discord! discord.gg/vJzqtew6nq",
+    type: "PLAYING",
+    time: 12
+    },{
+    text: "The world burn and come back",
+    type: "WATCHING",
+    time: 12
+    },{
+    text: "News about covid-19",
+    type: "LISTENING",
+    time: 12
+    }]
